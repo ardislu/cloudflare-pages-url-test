059a19c87bec6d047e7b64ce4325dae74ca1c2e9
Ardis Lu <ardisglu@gmail.com>
2022-07-31 20:13:58 -0700

    Make the response message clearer

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,8 @@
 export async function onRequestGet() {
   const exampleUrl = new URL('https://example.com');
 
-  const stringResponse = await fetch(exampleUrl.toString()); // Works both locally and when deployed
-  const urlResponse = await fetch(exampleUrl); // Throws an "Invalid URL" error when testing locally but not when deployed
+  await fetch(exampleUrl.toString()); // Works both locally and when deployed
+  await fetch(exampleUrl); // Throws an "Invalid URL" error when testing locally but works when deployed
 
-  return new Response(`stringResponse: ${stringResponse}\nurlResponse: ${urlResponse}`);
+  return new Response(`Both responses completed without throwing an error.`);
 }
